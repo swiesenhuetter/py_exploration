51d0211f245c874ee878d41462d55e8c3f7aa556
Stephan Wiesenhütter <wiesenhuetter@gmail.com>
2025-06-01 20:13:16 +0200

    this option disables usage of type hints in C code

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     name="My Math Module Cythonized",
     ext_modules=cythonize(
         "cython_test.py", # Compile the .py file
-        compiler_directives={'language_level': "3"},
+        compiler_directives={'language_level': "3",
+                             'annotation_typing': False
+                             },
         build_dir = 'cython_build')
 )
